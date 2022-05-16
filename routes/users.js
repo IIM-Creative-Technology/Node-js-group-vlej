88d20f10e81bb7a685b0d0fd5e1b325dbbd0af8c
Etienne Secondini <esecondini@dataswati.com>
2022-05-16 16:05:29 +0200

    fix error

--- routes/users.js
+++ routes/users.js
@@ -5,20 +5,17 @@ const client = require('../database')
 
 const db = client.db('nodejsDatabase');
 const usersCollection =  db.collection("Users");
-console.log(db.collection("Users"))
-
 /* GET users listing. */
+
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
 router.post('/create', function(req,res,next){
-
   usersCollection.insertOne(req.body)
   .then(result => {
     res.redirect('/')
   })
   .catch(error => console.error(error))
-  // res.json(req.query)
 });
 module.exports = router;
