a128ef38b6b6b8bf11cffdcac853185fc0e05386
Etienne Secondini <esecondini@dataswati.com>
2022-05-17 09:48:06 +0200

    remove template engine

--- app.js
+++ app.js
@@ -11,10 +11,6 @@ const bodyParser = require('body-parser');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(cookieParser());
@@ -40,7 +36,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({"error":err});
 });
 
 module.exports = app;
