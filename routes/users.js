9589c5ee4e25980c0711330765c3367f13f2dd81
Etienne Secondini <esecondini@dataswati.com>
2022-05-17 12:26:28 +0200

    create delete

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const client = require('../database')
-
+const UserModel = require('../models/users-model')
 
 const db = client.db('nodejsDatabase');
 const usersCollection =  db.collection("Users");
@@ -25,10 +25,17 @@ router.get('/:id', function(req, res, next) {
 });
 
 router.post('/create', function(req, res, next){
-  usersCollection.insertOne(req.body)
+  const user = new UserModel(req.body);
+  usersCollection.insertOne(user)
   .then(result => {
-    res.redirect('/')
+    res.json({ success: user.id })
   })
   .catch(error => console.error(error))
+});
+
+router.delete('/delete/:id', function (req, res) {
+  var id = req.params.id;
+  usersCollection.deleteOne({ id: id });
+  res.json({ success: id })
 });
 module.exports = router;
