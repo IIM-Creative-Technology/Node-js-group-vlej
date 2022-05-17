392b6a5aa5e50b844ecae2cfb301b279d7503c2f
Zhiwei-Zhu <55196933+Zhiwei-Zhu@users.noreply.github.com>
2022-05-17 14:13:25 +0200

    front view

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const path = require('path');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/',function(req,res){
+  console.log(__dirname+'../view/index.html')
+  res.sendFile(path.join(__dirname+'/../view/index.html'));
 });
+
 
 module.exports = router;
