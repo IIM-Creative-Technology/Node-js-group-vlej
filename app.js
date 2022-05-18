cf50213725e6568199fb9d91347e2b07e364cd1b
Zhiwei-Zhu <55196933+Zhiwei-Zhu@users.noreply.github.com>
2022-05-18 10:48:47 +0200

    initialize socketio

--- app.js
+++ app.js
@@ -15,9 +15,19 @@ var roomsRouter = require('./routes/rooms');
 var messagesRouter = require('./routes/messages');
 const { LoggerLevel } = require('mongodb');
 const bodyParser = require('body-parser');
+const http = require('http');
+const socketio = require("socket.io");
 
 // INITIALISATION
 var app = express();
+const server = http.createServer(app);
+const io =  socketio(server);
+
+//socket
+app.use(function(req, res, next){
+  res.io = io;
+  next();
+});
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -65,5 +75,9 @@ app.use(function(err, req, res, next) {
   res.json({"error":err});
 });
 
-module.exports = app;
+
+
+
+
+module.exports = {app: app, server:server};
 
--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,19 @@ const path = require('path');
 router.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/../view/index.html'));
 });
+router.get('/chatroom/:id',function(req,res){
+  res.io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+    socket.on('message', (msg) => {
+        console.log(msg)
+        res.io.emit('message',msg );
+      });
+  });
+
+  res.sendFile(path.join(__dirname+'/../view/chatroom.html'));
+});
 
 module.exports = router;
