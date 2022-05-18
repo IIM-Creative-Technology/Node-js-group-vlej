384e589edf72701665536a9da13fed0ba0b0409b
Etienne Secondini <esecondini@dataswati.com>
2022-05-18 11:18:00 +0200

    delte room from front

--- routes/rooms.js
+++ routes/rooms.js
@@ -32,7 +32,7 @@ router.post('/', function(req, res, next){
     res.json({success:true})
   })
   .catch(error => console.error(error))
-  
+
 });
 
 /* DELETE room by id */
@@ -42,4 +42,12 @@ router.delete('/:id', function (req, res) {
   res.json({ success: roomId })
 });
 
+/*Insert user id in room */
+router.put('/:id', function(req,res){
+  const roomId = parseInt(req.params.id)
+  roomsCollection.findOneAndUpdate(
+    { id: roomId },
+    { $push: { user_id: req.body.user_id } }
+  )
+})
 module.exports = router;
